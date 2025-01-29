c881a210943cb89f3ca31285a13326bd6afbfec4
Jun Jie Lin <105208525+JunJieLin1@users.noreply.github.com>
2025-01-29 02:06:32 +0100

    Navbar
    Aanmaak van een Navbar met een Logo toegevoegd.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,11 @@
+<template>
+  <nav class="bg-white shadow-md px-6 py-3 flex items-center justify-between">
+    <!-- Logo & Naam met link naar homepage -->
+    <router-link to="/" class="flex items-center space-x-2">
+      <img src="@/assets/panda-logo.png" alt="Logo" class="h-10 w-10 rounded-full shadow-md">
+      <span class="text-green-600 font-bold text-lg">Jun Jie</span>
+    </router-link>
+
+    <div class="flex space-x-6 text-gray-700 font-medium">
+      <router-link to="/helloworld" class="hover:text-blue-500">HelloWorld</router-link>
+    </div>
